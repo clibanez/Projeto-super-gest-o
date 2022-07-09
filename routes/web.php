648c37fa54d2cc9a66d1b23cc497a6ace37859e0
Clibanez <clibanezzcaldas@gmail.com>
2022-07-09 00:08:21 -0300

    Criação dos controllers e routes principal,sobre-nos e contato,

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,8 +14,24 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+
+Route::get('/',[PrincipalController::class,'principal'])->name('site.index');
+Route::get('/sobre-nos',[SobreNosController::class,'sobreNos'])->name('site.sobrenos');
+Route::get('/contato',[ContatoController::class,'contato'])->name('site.contato');
+Route::get('/login',[ContatoController::class,'contato'])->name('site.login');
+
+Route::prefix('/app')->group(function(){
+Route::get('/clientes',[ContatoController::class,'contato'])->name('app.cliente');
+Route::get('/fornecedores',[ContatoController::class,'contato'])->name('app.fornecedores');
+Route::get('/produtos',[ContatoController::class,'contato'])->name('app.produtos');
+});
+
+//Rota fallback
+Route::fallback(function() { echo 'A rota acessada Não existe. <a href="'.route('site.index').'">Clique Aqui</a> para ir para página principal';
 });
